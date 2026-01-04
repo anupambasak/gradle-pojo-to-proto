82f005532009614a5bff1e5196d6bd968405ee20
Anupam Basak <anupam.basak@gmail.com>
2026-01-04 18:46:02 +0530

    add the test dto

---
+++ test-pojo-to-proto/src/main/java/com/anupambasak/gradle/dtos/TimePojo.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2026 the project's contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.anupambasak.gradle.dtos;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class TimePojo {
+    private Instant instant;
+    private ZonedDateTime zonedDateTime;
+    private LocalDateTime localDateTime;
+    private LocalDate localDate;
+    private LocalTime localTime;
+    private Duration duration;
+    private Period period;
+    private List<Instant> instants;
+    private List<ZonedDateTime> zonedDateTimes;
+    private List<LocalDateTime> localDateTimes;
+    private List<LocalDate> localDates;
+    private List<LocalTime> localTimes;
+    private List<Duration> durations;
+    private List<Period> periods;
+}
